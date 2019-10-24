0fd504152dd7b5193d059823e7d4969625f22e36
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2019-10-25 00:08:46 +0300

    @srcemre'nin tespit ettiği gün hatası giderildi. UI tarafında input alanında yazılar ortalandı ve input değer uzunlukları güncellendi. #2

--- DayOfLife/app/src/main/java/com/muhammet/dayoflife/MainActivity.java
+++ DayOfLife/app/src/main/java/com/muhammet/dayoflife/MainActivity.java
@@ -2,23 +2,25 @@ package com.muhammet.dayoflife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.time.Month;
-import java.time.Year;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    EditText dateDayInput, dateMonthInput, dateYearInput;
+    EditText dateDayInput,
+             dateMonthInput,
+             dateYearInput;
+
     Button calculateButton;
+
     TextView output;
+
+    int[] bigMonths   = { 1, 3, 5, 7, 8, 10, 12 },
+          shortMonths = { 4, 6, 9, 11 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,50 +39,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCalculateButtonClicked(View view) {
-        int day          = Integer.parseInt(dateDayInput.getText().toString());
-        int month        = Integer.parseInt(dateMonthInput.getText().toString());
-        int year         = Integer.parseInt(dateYearInput.getText().toString());
-        int currentYear  = Calendar.getInstance().get(Calendar.YEAR);
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
-        int currentDay   = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        int totalDay = 0;
+        int daysCount  = howManyDaysLived();
+        String message = String.format(getString(R.string.livedDaysCount), daysCount);
 
-        for(; year < currentYear; year++) {
-            if ( year % 4 == 0 )
+
+        output.setText(message);
+    }
+
+    private boolean isInclude(int[] array, int value) {
+        for ( int item : array )
+            if ( item == value )
+                return true;
+
+        return false;
+    }
+
+    private int howManyDaysLived() {
+        Calendar calendar = Calendar.getInstance();
+
+        int birthDay     = Integer.parseInt(dateDayInput.getText().toString()),
+            birthMonth   = Integer.parseInt(dateMonthInput.getText().toString()),
+            birthYear    = Integer.parseInt(dateYearInput.getText().toString()),
+            currentYear  = calendar.get(Calendar.YEAR),
+            currentMonth = calendar.get(Calendar.MONTH) + 1,
+            currentDay   = calendar.get(Calendar.DAY_OF_MONTH),
+            totalDay     = 0,
+            temporaryCounter;
+
+        totalDay -= birthDay;
+
+        for ( temporaryCounter = 1; temporaryCounter < birthMonth; temporaryCounter++ )
+            totalDay -= howManyDaysHaveMonth(temporaryCounter, birthYear);
+
+        for ( temporaryCounter = birthYear; temporaryCounter < currentYear; temporaryCounter++ )
+            if ( temporaryCounter % 4 == 0 )
                 totalDay += 366;
             else
                 totalDay += 365;
-        }
 
-        Integer[] bigMonths   = { 1, 3, 5, 7, 8, 10, 12 };
-        Integer[] shortMonths = { 4, 6, 9, 11 };
-
-
-        for(; month < currentMonth; month++) {
-            if ( isInclude(bigMonths, month) )
-                totalDay += 31;
-            else if ( isInclude(shortMonths, month) )
-                totalDay += 30;
-            else
-                if ( year % 4 == 0 )
-                    totalDay += 29;
-                else
-                    totalDay += 28;
-        }
+        for ( temporaryCounter = 1; temporaryCounter < currentMonth; temporaryCounter++ )
+            totalDay += howManyDaysHaveMonth(temporaryCounter, currentYear);
 
         totalDay += currentDay;
 
-        output.setText("Toplam Yaşadığınız Gün Sayısı: " + Integer.toString(totalDay));
+        return totalDay;
     }
 
-    private boolean isInclude(Integer[] array, int value) {
-        Integer counter;
+    private int howManyDaysHaveMonth(int month, int year) {
+        if ( isInclude(bigMonths, month) )   return 31;
+        if ( isInclude(shortMonths, month) ) return 30;
+        if ( year % 4 == 0 )                 return 29;
 
-        for (counter = 0; counter < array.length; counter++) {
-            if ( array[counter] == value )
-                return true;
-        }
-
-        return false;
+        return 28;
     }
 }
