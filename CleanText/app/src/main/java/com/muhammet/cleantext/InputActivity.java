7d92d0ff4b82c59bf548d11595c98e83c38fc56a
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2019-10-31 18:52:11 +0300

    Clean Text ekran çıktısı eklendi

---
+++ CleanText/app/src/main/java/com/muhammet/cleantext/InputActivity.java
@@ -0,0 +1,14 @@
+package com.muhammet.cleantext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
